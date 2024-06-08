248a480fa2fcd6beeade2b69414517a69847eb7d
rosonerri <gabrieldotajayi4real@gmail.com>
2024-06-08 23:50:10 +0100

    First Push

---
+++ src/router/studentRouter.tsx
@@ -0,0 +1,55 @@
+import { createBrowserRouter } from "react-router-dom";
+import StudentDashboard from "../pagesForStudents/StudentDashboard";
+// import Layout from "../components/layout/Layout";
+import Assignment from "../pagesForStudents/Assignment";
+import MySubjects from "../pagesForStudents/MySubjects";
+import Article from "../pagesForStudents/Article";
+import TimeTable from "../pagesForStudents/TimeTable";
+import StudentProfile from "../pagesForStudents/StudentProfile";
+import Layout from "../pagesForStudents/layout/Layout";
+import MyClassroom from "../pagesForStudents/pages/subjects/Subject";
+import QuizSetupScreen from "../pagesForStudents/pages/quiz/QuizSetupScreen";
+import QuizTestScreen from "../pagesForStudents/pages/quiz/QuizTestScreen";
+import MyClassRoomScreen from "../pagesForStudents/pages/class/MyClassRoom";
+
+export const studentRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <StudentDashboard /> },
+      {
+        path: "/time-table",
+        element: <TimeTable />,
+      },
+      {
+        path: "/your-profile",
+        element: <StudentProfile />,
+      },
+      {
+        path: "/my-classroom",
+        element: <MyClassRoomScreen />,
+      },
+      {
+        path: "/articles",
+        element: <Article />,
+      },
+      {
+        path: "/your-subjects",
+        element: <MyClassroom />,
+      },
+      {
+        path: "subjects/:subjectID",
+        element: <QuizSetupScreen />,
+      },
+      {
+        path: "/quiz/details/:quizID",
+        element: <QuizTestScreen />,
+      },
+      {
+        path: "/assignment",
+        element: <Assignment />,
+      },
+    ],
+  },
+]);
